e8f03892f0a5d9891c98150742e3fa8f043d3f66
yumauda <firststep-yumauda@gmail.com>
2025-12-24 22:09:23 +0900

    aboutページ2セクション分

--- page-about.php
+++ page-about.php
@@ -47,7 +47,7 @@
             <p class="p-secret__list-text">石州瓦では長石中に気泡と思われる半球状のくぼみが多数観察された。長石の融点以上の1200℃近傍で焼成することにより<span>長石が溶融</span>し、周辺のムライトと良好な接合をし<span>空隙の少ない緻密な組織</span>となている。</p>
             <p class="p-secret__small">〜島根県産業技術センター論文より</p>
           </li>
-          
+
         </ul>
       </div>
     </div>
@@ -63,6 +63,54 @@
         </div>
         <figure class="p-land__img">
           <img src="<?php echo get_template_directory_uri() ?>/images/about/land_1.webp" alt="島根県産の原料" width="900" height="980">
+        </figure>
+      </div>
+    </div>
+  </section>
+
+  <section class="p-features">
+    <div class="l-inner">
+      <div class="p-features__content">
+        <div class="p-features__card">
+          <div class="p-features__card-detail">
+            <div class="p-features__block">
+              <p class="p-features__card-label">高温焼成</p>
+              <h3 class="p-features__card-title">高耐火の<br>原料</h3>
+            </div>
+            <figure class="p-features__card-img">
+              <img src="<?php echo get_template_directory_uri() ?>/images/about/feature_1.webp" alt="高耐火の原料" width="407" height="196">
+            </figure>
+          </div>
+          <p class="p-features__card-text">石州瓦の原料は耐火度が非常に高く、アルカリ分が少ないカオリサイトやハロイサイトなどの高耐火度の粘土鉱物の割合が多いため、<span>１２００℃以上の高温焼成</span>にも耐えられます。</p>
+        </div>
+        <div class="p-features__card">
+          <div class="p-features__card-detail">
+            <div class="p-features__block">
+              <p class="p-features__card-label">高密度な構造</p>
+              <h3 class="p-features__card-title">圧倒的な<br>耐久性能</h3>
+            </div>
+            <figure class="p-features__card-img">
+              <img src="<?php echo get_template_directory_uri() ?>/images/about/feature_2.webp" alt="圧倒的な耐久性能" width="407" height="196">
+            </figure>
+          </div>
+          <p class="p-features__card-text">高温焼成により粘土に含まれる長石が溶融し、<span>緻密な構造</span>が形成され、高い強度と耐久性を実現。<span>酸性雨</span>や<span>塩害</span>、<span>凍害</span>に非常に強い瓦です。</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+  <section class="p-compare">
+    <div class="l-inner">
+      <div class="p-compare__content">
+        <div class="p-compare__top">
+          <div class="p-compare__title">
+            <p class="p-compare__title-text">屋根材比較</p>
+            <h3 class="p-compare__title-main">他の屋根材と比べて、<br>陶器瓦はどう違う？</h3>
+          </div>
+          <p class="p-compare__text">瓦にもさまざまな種類があり、それぞれに特長があります。<br>他の屋根材と陶器瓦を比較しながら、その違いや強みをご紹介します。</p>
+        </div>
+        <figure class="p-compare__img">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/about/compare_graph.webp" alt="他の屋根材と陶器瓦を比較" width="1520" height="584">
         </figure>
       </div>
     </div>
