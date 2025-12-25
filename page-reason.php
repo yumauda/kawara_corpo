874fd1b9d59a1fd5e58a2a527ae04cd4667e3e9a
yumauda <firststep-yumauda@gmail.com>
2025-12-25 16:53:02 +0900

    選ばれる理由途中まで

---
+++ page-reason.php
@@ -0,0 +1,63 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail" data-title="Strong">
+          <h2 class="p-page-mv__title">瓦百景が選ばれる理由</h2>
+          <p class="p-page-mv__text">The reason why KAWARA is chosen</p>
+        </div>
+        <figure class="p-page-mv__img">
+          <img src="<?php echo get_template_directory_uri() ?>/images/reason/mv_strong.webp" alt="瓦百景が選ばれる理由" width="1400" height="540">
+        </figure>
+        <div class="p-page-mv__bottom">
+          <p class="p-page-mv__bottom-kind">選ばれる理由</p>
+          <h3 class="p-page-mv__bottom-title">潮風に負けない<br>石州瓦という選択</h3>
+        </div>
+        <div class="p-page-mv__explain">
+          <p class="p-page-mv__explain-title">石州瓦が島国の住まいを守る</p>
+          <p class="p-page-mv__explain-text">海辺に住む住宅では、空気中の塩分が建材を腐食させる「塩害」が深刻な課題です。<br>
+            金属だけでなく、屋根材も長年かけて劣化し、雨漏りやひび割れなどの原因となります。
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <section class="p-beautiful">
+    <div class="l-inner">
+      <div class="p-beautiful__content">
+        <div class="p-beautiful__left">
+          <div class="p-beautiful__top">
+            <h3 class="p-beautiful__title">強さは美しさ。<br>だから私の選択は石州瓦。</h3>
+            <p class="p-beautiful__text">石州瓦は1200℃超の高温焼成でつくられ、塩害や寒暖差に強く、色あせや劣化が少ない屋根材です。<br>
+              塗装や特別なメンテナンスが不要で、防災性・耐凍害性にも優れているため、沿岸部や寒冷地でも安心して使えます。<br>
+              耐久性・耐候性・防火性のすべてに優れ、長く美観と安全を守れる瓦として、全国のプロから高く評価されています。</p>
+          </div>
+          <div class="p-beautiful__second">
+            <p class="p-beautiful__second-title">瓦百景は凍害・塩害も保証します</p>
+            <div class="p-beautiful__row">
+              <figure class="p-beautiful__row-img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/reason/beautiful_icon.webp" alt="凍害・塩害も保証" width="260" height="253">
+              </figure>
+              <p class="p-beautiful__text">凍害・塩害による製品の劣化や損傷に対して、10年間の製品保証をお約束いたします。<br>
+                これは石州瓦の1200℃超高温焼成による圧倒的な耐久性能への確信の証。<br>
+                お客様の大切な住まいを、確かな品質と万全の保証体制で守り抜きます。</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-beautiful__right">
+          <figure class="p-beautiful__img">
+            <img src="<?php echo get_template_directory_uri() ?>/images/reason/beautiful_img.webp" alt="石州瓦を選ぶ理由" width="790" height="834">
+          </figure>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+  <?php get_template_part("includes/submit"); ?>
+
+</main>
+
+<?php get_footer() ?>
