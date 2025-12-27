18cd9cf77c2b2c6faf6bb1ce1c35666b15ad3002
yumauda <firststep-yumauda@gmail.com>
2025-12-27 15:46:43 +0900

    商品紹介ページ一覧

---
+++ page-product.php
@@ -0,0 +1,249 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv p-page-mv--company">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail" data-title="Product">
+          <h2 class="p-page-mv__title">商品紹介</h2>
+          <p class="p-page-mv__text">Build with Pride</p>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <section class="p-product">
+    <div class="l-inner">
+      <div class="p-product__content">
+        <div class="p-product__title">
+          <h3 class="c-pageTitle">
+            <span class="c-pageTitle__title-text">瓦製品</span>
+            <span class="c-pageTitle__title-main">大切な建物を守る、<br>風土が生み出した石州瓦。</span>
+          </h3>
+        </div>
+        <div class="p-product__block-wrapper">
+          <div class="p-product__block">
+            <div class="p-product__left">
+              <div class="p-product__icon">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_icon1.png" alt="グラスロ" width="274" height="100">
+              </div>
+              <ul class="p-product__lists">
+                <li class="p-product__list">
+                  超低勾配対応
+                </li>
+                <li class="p-product__list">
+                  耐震機能
+                </li>
+                <li class="p-product__list">
+                  耐風機能
+                </li>
+              </ul>
+              <h4 class="p-product__subTitle">グラスロは、屋根勾配を<br>
+                選ばないJ形瓦です。</h4>
+              <div class="p-product__btn">
+                <a href="#" class="c-btn">
+                  <p class="c-btn__text">View more</p>
+                  <div class="c-btn__icon">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/btn_icon.svg" alt="" width="60" height="60">
+                  </div>
+
+                </a>
+              </div>
+            </div>
+            <figure class="p-product__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_img1.webp" alt="グラスロ" width="1100" height="452">
+            </figure>
+          </div>
+          <div class="p-product__block">
+            <div class="p-product__left">
+              <div class="p-product__icon p-product__icon--240">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_icon2.png" alt="BSルーフ" width="240" height="80">
+              </div>
+              <ul class="p-product__lists">
+                <li class="p-product__list">
+                  防水機能
+                </li>
+                <li class="p-product__list">
+                  耐震機能
+                </li>
+                <li class="p-product__list">
+                  耐風機能
+                </li>
+              </ul>
+              <h4 class="p-product__subTitle p-product__subTitle--bs">建築の未来を支える<br>グローバルスタンダード瓦。</h4>
+              <div class="p-product__btn">
+                <a href="#" class="c-btn">
+                  <p class="c-btn__text">View more</p>
+                  <div class="c-btn__icon">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/btn_icon.svg" alt="" width="60" height="60">
+                  </div>
+
+                </a>
+              </div>
+            </div>
+            <figure class="p-product__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_img2.webp" alt="BSルーフ" width="1100" height="452">
+            </figure>
+          </div>
+          <div class="p-product__block">
+            <div class="p-product__left">
+              <div class="p-product__icon p-product__icon--300">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_icon4.png" alt="フレキシブルな石州瓦" width="240" height="80">
+              </div>
+              <ul class="p-product__lists">
+                <li class="p-product__list">
+                  超低勾配対応
+                </li>
+                <li class="p-product__list">
+                  耐震機能
+                </li>
+                <li class="p-product__list">
+                  耐風機能
+                </li>
+
+              </ul>
+              <h4 class="p-product__subTitle p-product__subTitle--flex">建築様式を選ばない<br>
+                フレキシブルな石州瓦</h4>
+              <div class="p-product__btn">
+                <a href="#" class="c-btn">
+                  <p class="c-btn__text">View more</p>
+                  <div class="c-btn__icon">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/btn_icon.svg" alt="" width="60" height="60">
+                  </div>
+
+                </a>
+              </div>
+            </div>
+            <figure class="p-product__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_img4.webp" alt="フレキシブルな石州瓦" width="1100" height="452">
+            </figure>
+          </div>
+          <div class="p-product__block">
+            <div class="p-product__left">
+              <div class="p-product__icon p-product__icon--362">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_icon3.png" alt="フラット瓦" width="240" height="80">
+              </div>
+              <ul class="p-product__lists">
+                <li class="p-product__list">
+                  防音性能
+                </li>
+                <li class="p-product__list">
+                  経済性
+                </li>
+                <li class="p-product__list">
+                  オプション
+                </li>
+                <li class="p-product__list">
+                  互換性
+                </li>
+              </ul>
+              <h4 class="p-product__subTitle p-product__subTitle--flat">和からモダンまで<br>
+                幅広く対応するフラット瓦</h4>
+              <div class="p-product__btn">
+                <a href="#" class="c-btn">
+                  <p class="c-btn__text">View more</p>
+                  <div class="c-btn__icon">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/btn_icon.svg" alt="" width="60" height="60">
+                  </div>
+
+                </a>
+              </div>
+            </div>
+            <figure class="p-product__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_img3.webp" alt="フラット瓦" width="1100" height="452">
+            </figure>
+          </div>
+          <div class="p-product__block">
+            <div class="p-product__left">
+              <div class="p-product__icon p-product__icon--388">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_icon5.png" alt="S形瓦" width="240" height="80">
+              </div>
+              <ul class="p-product__lists">
+                <li class="p-product__list">
+                  防災性能
+                </li>
+                <li class="p-product__list">
+                  防水瀬能
+                </li>
+
+              </ul>
+              <h4 class="p-product__subTitle p-product__subTitle--s">石州瓦の技と感性が生んだ<br>
+                S形瓦</h4>
+              <div class="p-product__btn">
+                <a href="#" class="c-btn">
+                  <p class="c-btn__text">View more</p>
+                  <div class="c-btn__icon">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/btn_icon.svg" alt="" width="60" height="60">
+                  </div>
+
+                </a>
+              </div>
+            </div>
+            <figure class="p-product__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_img5.webp" alt="S形瓦" width="1100" height="452">
+            </figure>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <section class="p-product">
+    <div class="l-inner">
+      <div class="p-product__content">
+        <div class="p-product__title">
+          <h3 class="c-pageTitle">
+            <span class="c-pageTitle__title-text">副資材・その他</span>
+            <span class="c-pageTitle__title-main">規格外の瓦に付加価値のある<br>
+              リサイクル商品として再生させる。</span>
+          </h3>
+        </div>
+        <div class="p-product__block-wrapper">
+          <div class="p-product__block">
+            <div class="p-product__left">
+              <div class="p-product__icon p-product__icon--300">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_icon6.png" alt="庭の敷砂" width="274" height="100">
+              </div>
+              <ul class="p-product__lists">
+                <li class="p-product__list">
+                  庭の敷砂
+                </li>
+                <li class="p-product__list">
+                  園芸用土壌の被覆
+                </li>
+                <li class="p-product__list">
+                  駐車場
+                </li>
+              </ul>
+              <h4 class="p-product__subTitle">自然な色合いの瓦素地が<br>
+                様々な景観にマッチ</h4>
+              <div class="p-product__btn">
+                <a href="#" class="c-btn">
+                  <p class="c-btn__text">View more</p>
+                  <div class="c-btn__icon">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/btn_icon.svg" alt="" width="60" height="60">
+                  </div>
+
+                </a>
+              </div>
+            </div>
+            <figure class="p-product__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_img6.webp" alt="庭の敷砂" width="1100" height="452">
+            </figure>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+  <?php get_template_part("includes/submit"); ?>
+
+</main>
+
+<?php get_footer() ?>
