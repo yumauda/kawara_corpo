af5622b19d37e1749d02d5e1a90cdc6bf1df638c
yumauda <firststep-yumauda@gmail.com>
2025-12-29 23:19:04 +0900

    bsページとtsページPC

---
+++ page-bs.php
@@ -0,0 +1,264 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-product-mv">
+    <div class="l-inner">
+      <div class="p-product-mv__content">
+        <div class="p-product-mv__top">
+          <figure class="p-product-mv__icon w300">
+            <img src="<?php echo get_template_directory_uri() ?>/images/product/product_icon2.png" alt="BSルーフ" width="274" height="100">
+          </figure>
+          <h2 class="p-product-mv__title">BSルーフ 石州防災J形瓦 53A</h2>
+        </div>
+        <figure class="p-product-mv__img">
+          <img src="<?php echo get_template_directory_uri() ?>/images/bs/page-mv.webp" alt="BSルーフ" width="1840" height="540">
+        </figure>
+        <div class="p-product-mv__detail">
+          <h3 class="p-product-mv__detail-title green">あらゆる屋根に、<br>新しい瓦の可能性を。</h3>
+          <p class="p-product-mv__detail-text">建築の未来を支えるグローバルスタンダード瓦。</p>
+          <p class="p-product-mv__detail-small">BSルーフは、石州瓦の中で唯一、JIS-53A規格を満たしています。<br>
+            耐震・耐風・防水・防火・遮音性に優れ、あらゆる建築様式に対応できる豊富な部材を備えたグローバルスタンダードです。</p>
+        </div>
+
+
+      </div>
+    </div>
+  </section>
+  <section class="p-intro">
+    <div class="l-inner">
+      <div class="p-intro__content">
+        <div class="p-intro__vertical">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/graslo/intro_vertical.png" alt="強さは美しさ" width="50" height="460">
+        </div>
+        <div class="p-intro__detail">
+          <h3 class="p-intro__title"><span>高耐久性</span><br>で安心。</h3>
+          <p class="p-intro__en">BSルーフ</p>
+          <p class="p-intro__small">Disaster-resistant Sekishu roof tiles.</p>
+          <div class="p-intro__row">
+            <div class="p-intro__icon">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/product/product_icon2.png" alt="BSルーフ" width="220" height="80">
+            </div>
+            <p class="p-intro__row-text">BSルーフ<br>石州防災J形瓦 53A</p>
+          </div>
+        </div>
+        <figure class="p-intro__img">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/bs/intro_img.webp" alt="BSルーフ" width="1040" height="677">
+        </figure>
+      </div>
+    </div>
+  </section>
+  <section class="p-product-features">
+    <div class="l-inner">
+      <div class="p-product-features__content">
+        <div class="p-product-features__title-wrapper">
+          <div class="p-product-features__title">
+            <h3 class="c-pageTitle">
+              <span class="c-pageTitle__title-text green">屋根材比較</span>
+              <span class="c-pageTitle__title-main">和の趣に<br>信頼の<br>防災性能。</span>
+            </h3>
+          </div>
+          <div class="p-product-features__detail">
+            <p class="p-product-features__detail-text">伝統に寄り添う、<br>災害に強い日本の瓦。</p>
+            <p class="p-product-features__detail-small">日本建築をさらに格調高く仕上げる<br>滑らかなフォルム。<br>優れた防災性能を備えた和瓦です。</p>
+          </div>
+        </div>
+
+        <!-- BSルーフの機能カード -->
+        <div class="p-product-features__cards">
+          <!-- 01 防水機能 -->
+          <div class="p-product-features__card p-product-features__card--green p-product-features__card--wide">
+            <div class="p-product-features__card-left">
+              <div class="p-product-features__card-header">
+                <span class="p-product-features__card-number p-product-features__card-number--white">01</span>
+                <h3 class="p-product-features__card-title p-product-features__card-title--white">防水機能</h3>
+              </div>
+              <div class="p-product-features__card-line p-product-features__card-line--white"></div>
+              <p class="p-product-features__card-text p-product-features__card-text--white">四段の水返しで縦方向の逆流と二重の水切りで横方向の漏れを同時に防止します。</p>
+            </div>
+            <figure class="p-product-features__card-img-large">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/bs/waterproof.png" alt="防水機能" width="240" height="230">
+            </figure>
+            <span class="p-product-features__card-slope p-product-features__card-slope--white">_Water</span>
+          </div>
+
+          <div class="p-product-features__grid-two">
+            <!-- 02 耐震機能 -->
+            <div class="p-product-features__card p-product-features__card--green">
+              <div class="p-product-features__card-header">
+                <span class="p-product-features__card-number p-product-features__card-number--white">02</span>
+                <h3 class="p-product-features__card-title p-product-features__card-title--white">耐震機能</h3>
+              </div>
+              <div class="p-product-features__card-line p-product-features__card-line--white"></div>
+              <p class="p-product-features__card-text p-product-features__card-text--white">裏面に設置した安定駒が瓦同士の押さえ込みを強化して固定力を発揮。強風や地震の揺れによる瓦のズレを効果的に防止します。</p>
+              <figure class="p-product-features__card-img-large--second">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/bs/earthquake.png" alt="耐震機能" width="260" height="180">
+              </figure>
+              <span class="p-product-features__card-slope p-product-features__card-slope--white">_Quake</span>
+            </div>
+
+            <!-- 03 耐風機能 -->
+            <div class="p-product-features__card p-product-features__card--white fit-content">
+              <div class="p-product-features__card-header">
+                <span class="p-product-features__card-number p-product-features__card-number--green">03</span>
+                <h3 class="p-product-features__card-title p-product-features__card-title--green">耐風機能</h3>
+              </div>
+              <div class="p-product-features__card-line p-product-features__card-line--green"></div>
+              <p class="p-product-features__card-text">瓦同士の合体構造により強風による瓦の浮き上がりを防ぎ、台風などの過酷な環境下でも問題なく住宅を守り続けます。</p>
+              <div class="p-product-features__card-detail">
+                <figure class="p-product-features__card-img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/bs/wind.png" alt="合体構造" width="100" height="90">
+                </figure>
+                <p class="p-product-features__card-caption">合体構造(黄)により左後方の瓦が押さえ込まれ浮き上がりを防止</p>
+              </div>
+              <span class="p-product-features__card-slope p-product-features__card-slope--green">_Wind</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  
+  <section class="p-specific p-specific--first">
+    <div class="l-inner">
+      <div class="p-specific__header green-line">
+        <div class="p-specific__header-bar green-bg"></div>
+        <h2 class="p-specific__header-title green">色展開</h2>
+      </div>
+      <div class="p-specific__content">
+        <figure class="p-specific__color">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/bs/color.webp" alt="色展開" width="1400" height="613">
+        </figure>
+      </div>
+    </div>
+  </section>
+  <section class="p-specific">
+    <div class="l-inner">
+      <div class="p-specific__header green-line">
+        <div class="p-specific__header-bar green-bg"></div>
+        <h2 class="p-specific__header-title green">製品仕様</h2>
+      </div>
+      <div class="p-specific__content">
+        <!-- 製品図面と仕様 -->
+        <div class="p-specific__top">
+          <div class="p-specific__diagram">
+            <figure class="p-specific__diagram-img mix-brend">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/bs/diagram.png" alt="製品図面" width="440" height="440">
+            </figure>
+          </div>
+          <div class="p-specific__specs">
+            <h3 class="p-specific__specs-title">■製品仕様</h3>
+            <table class="p-specific__table">
+              <tr>
+                <th class="border-left-none w150" rowspan="4">寸法</th>
+                <th>全長さ</th>
+                <td class="border-right-none">305㎜(±4㎜)</td>
+              </tr>
+              <tr>
+                <th>全幅</th>
+                <td class="border-right-none">305㎜(±4㎜)</td>
+              </tr>
+              <tr>
+                <th>働き長さ</th>
+                <td class="border-right-none">235㎜(±4㎜)</td>
+              </tr>
+              <tr>
+                <th>働き幅</th>
+                <td class="border-right-none">265㎜(±4㎜)</td>
+              </tr>
+              <tr>
+                <th class="border-left-none">葺き枚数</th>
+                <td class="border-right-none" colspan="2">16枚/㎡・53枚/坪</td>
+              </tr>
+              <tr>
+                <th class="border-left-none">重量</th>
+                <td class="border-right-none" colspan="2">2.7㎏/枚・43.2㎏/㎡</td>
+              </tr>
+            </table>
+            <p class="p-specific__note">※瓦の割付をする場合、現物で寸法を確認し割付を行ってください。</p>
+          </div>
+          <div class="p-specific__slope">
+            <h3 class="p-specific__slope-title">■標準屋根勾配と流れ長さ</h3>
+            <table class="p-specific__slope-table">
+              <tr>
+                <th class="border-left-none">勾配</th>
+                <th class="border-right-none">長さ(ｍ)</th>
+              </tr>
+              <tr>
+                <td class="border-left-none">3.0</td>
+                <td class="border-right-none">6</td>
+              </tr>
+              <tr>
+                <td class="border-left-none">3.5</td>
+                <td class="border-right-none">8</td>
+              </tr>
+              <tr>
+                <td class="border-left-none">4.0</td>
+                <td class="border-right-none">10</td>
+              </tr>
+              <tr>
+                <td class="border-left-none">4.5</td>
+                <td class="border-right-none">12</td>
+              </tr>
+              <tr>
+                <td class="border-left-none">5.0</td>
+                <td class="border-right-none">14</td>
+              </tr>
+              <tr>
+                <td class="border-left-none">5.5</td>
+                <td class="border-right-none">16</td>
+              </tr>
+              <tr>
+                <td class="border-left-none">6.0</td>
+                <td class="border-right-none">18</td>
+              </tr>
+            </table>
+            <p class="p-specific__note">※この数値は、保証値ではありません。 設計に際しては、地域特性や建築物の形状等に留意してください。</p>
+          </div>
+        </div>
+
+        <!-- 桁行き方向と流れ方向 -->
+        <figure class="p-specific__diagrams">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/bs/direction.png" alt="桁行き方向と流れ方向" width="1400" height="400">
+        </figure>
+
+        <!-- 注意事項 -->
+        <div class="p-specific__notes">
+          <div class="p-specific__notes-box">
+            <h3 class="p-specific__notes-title">&lt; 安全に関するご注意 &gt;</h3>
+            <ul class="p-specific__notes-list">
+              <li>粘土瓦の施工には専門の工事が必要です。<br>専門工事業者にご相談ください。工事に不備があると、落下・雨漏り等の原因になることがあります。</li>
+              <li>寒冷積雪地域或いは強風地域では、特殊な工事を必要とする場合があります。こうした地域で粘土瓦を使う場合には、専門工事業者にご相談ください。</li>
+              <li>粘土瓦を使った屋根は、関連する工事標準仕様書に基づいて設計してください。特殊な設計を行う場合は、専門工事業者にご相談ください。</li>
+            </ul>
+          </div>
+          <div class="p-specific__notes-box">
+            <h3 class="p-specific__notes-title">&lt; 粘土瓦の特性について &gt;</h3>
+            <p class="p-specific__notes-intro">粘土瓦は天然素材から製造されるため、下記のような現象が発生する場合がありますが、製品欠陥ではなく品質上の問題はまったくありませんので安心してご使用ください。</p>
+            <ul class="p-specific__notes-list">
+              <li class="p-specific__notes-li"><strong>【色　ム　ラ】</strong>
+                <p class="p-specific__notes-text">粘土成分の微妙な違いや、焼成窯内の環境の変化により微妙な色合いが発生することがありますが品質上の問題はありません。
+              </li>
+              <li class="p-specific__notes-li"><strong>【貫　　　入】</strong>
+                <p class="p-specific__notes-text">釉薬表面に発生する細かい亀裂を貫入といいます。これは、粘土と釉薬の収縮率の違いから生まれ品質上の問題はありません。
+              </li>
+              <li class="p-specific__notes-li"><strong>【ピンホール】</strong>
+                <p class="p-specific__notes-text">釉薬表面に発生する小さなへこみをピンホールといいます。これは、釉薬の気泡や粘土に含まれている有機物が焼成によって消滅することで生まれますが品質上の問題はありません。
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+
+  <?php get_template_part("includes/submit"); ?>
+
+</main>
+
+<?php get_footer() ?>
