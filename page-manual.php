0f8964649f558e808ecdfd458e39b066b018300e
yumauda <firststep-yumauda@gmail.com>
2026-01-05 13:51:25 +0900

    施工マニュアルページ

---
+++ page-manual.php
@@ -0,0 +1,392 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv p-page-mv--download">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail" data-title="Technical">
+          <h2 class="p-page-mv__title">施工マニュアル</h2>
+          <p class="p-page-mv__text">For Professional</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="p-page-mvText">
+    <div class="l-inner">
+      <p class="p-page-mvText__content">
+      <p class="p-page-mvText__main">現在配布しています瓦製品カタログ、リーフレットをPDFファイル形式で記載しています。<br>
+        ダウンロードしてご活用ください。</p>
+      <p class="p-page-mvText__small">※PDFファイルをご覧いただくためにはアドビ社のAdobe Reader等の専用ソフトが必要になる場合があります</p>
+      </p>
+    </div>
+  </div>
+
+  <section class="p-leaflet p-leaflet--manual">
+    <div class="l-inner">
+      <div class="p-leaflet__content p-leaflet__content--manual">
+        <div class="p-leaflet__block">
+          <h3 class="p-leaflet__block-title">標準設計・施工ガイドライン</h3>
+          <div class="p-leaflet__cards">
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual1.png" alt="ダイジェスト版 補足付き" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">ダイジェスト版<br><span>(補足付き)</span></h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+
+          </div>
+        </div>
+        <div class="p-leaflet__block">
+          <h3 class="p-leaflet__block-title">標準マニュアル</h3>
+          <div class="p-leaflet__cards">
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual2.png" alt="標準施工要領書 抜粋 J53" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">標準施工要領書<br>抜粋 J53</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual3.png" alt="標準施工要領書 J53B" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">標準施工要領書<br>J53B</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual4.png" alt="標準施工要領書 J53A" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">標準施工要領書<br>J53A</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+
+          </div>
+
+          <div class="p-leaflet__note">
+            <div class="p-leaflet__note-section">
+              <h4 class="p-leaflet__note-title">&lt; 記載内容&gt;</h4>
+              <p class="p-leaflet__note-text">建築工事標準仕様に基づく施工方法の紹介致します。この施エマニュアルは（社）日本建築学会が、建築の質的向上と合理化を図る目的で定めた"建築工事標準仕様書（ＪＡＳＳ）12屋根工事"に従って解説したものです。</p>
+              <p class="p-leaflet__note-text">瓦屋根標準設計・施工ガイドラインに沿った工法の、設計に一般的に必要な製品概要・施工用材・工法を記載しています。</p>
+              <p class="p-leaflet__note-text">当データのご使用前に必ず、注意事項・特記事項を確認し承認した上でダウンロードしてください。</p>
+            </div>
+            <div class="p-leaflet__note-divider"></div>
+            <div class="p-leaflet__note-section">
+              <h4 class="p-leaflet__note-title">&lt; 注意・特記事項&gt;</h4>
+              <p class="p-leaflet__note-text">製品の特性上、製品サイズには多少のばらつきが発生いたします。</p>
+              <p class="p-leaflet__note-text">図面上のサイズと実際のサイズには多少の寸法差が発生することがあります。</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="p-leaflet__block">
+          <h3 class="p-leaflet__block-title">和瓦</h3>
+          <div class="p-leaflet__cards">
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual5.png" alt="TSルーフ53B" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">TSルーフ53B</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual6.png" alt="TSルーフ53B利休" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">TSルーフ53B利休</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual7.png" alt="グラスロ" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">グラスロ</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual8.png" alt="BSルーフ53A" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">BSルーフ53A</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual9.png" alt="MBK工法" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">MBK工法</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual10.png" alt="MBK壁際工法" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">MBK壁際工法</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual11.png" alt="セフティ防災棟工法" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">セフティ防災棟工法</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+
+
+          </div>
+
+        </div>
+        <div class="p-leaflet__block">
+          <h3 class="p-leaflet__block-title">洋瓦</h3>
+          <div class="p-leaflet__cards">
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual12.png" alt="ニューセラECOⅡ" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">ニューセラECOⅡ</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+          </div>
+
+        </div>
+        <div class="p-leaflet__block">
+          <h3 class="p-leaflet__block-title">板瓦</h3>
+          <div class="p-leaflet__cards">
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual13.png" alt="FtypeU" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">ニューセラFUタイプ</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+          </div>
+
+        </div>
+        <div class="p-leaflet__block">
+          <h3 class="p-leaflet__block-title">生産終了品</h3>
+          <div class="p-leaflet__cards">
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual14.png" alt="ニューセラECO" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">ニューセラECO</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual15.png" alt="ニューセラFFタイプ" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">ニューセラFFタイプ</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual16.png" alt="ニューセラFFタイプ システム工法" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">ニューセラFFタイプ<br>
+                  システム工法</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual17.png" alt="46判" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">46判</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual18.png" alt="レインシャット" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">レインシャット</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual19.png" alt="セラミカ セフティルーフ" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">セラミカ<br>
+                  セフティルーフ</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image p-leaflet__card-image--manual">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/manual/manual20.png" alt="ニューセラＳ" width="141" height="200">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <h4 class="p-leaflet__card-title">ニューセラＳ</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+          </div>
+          <p class="p-leaflet__attention">
+            ※生産終了時点でのマニュアルを掲載しており、修正・変更等は行っておりません。
+          </p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php get_template_part("includes/submit"); ?>
+
+</main>
+
+<?php get_footer() ?>
