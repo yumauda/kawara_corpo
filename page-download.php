fc074b132694ef589f758a8567803030c9e8c2ed
yumauda <firststep-yumauda@gmail.com>
2026-01-05 11:28:47 +0900

    資料請求ページ

---
+++ page-download.php
@@ -0,0 +1,381 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv p-page-mv--download">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail" data-title="Download">
+          <h2 class="p-page-mv__title">資料請求</h2>
+          <p class="p-page-mv__text">Catalog Download</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="p-page-mvText">
+    <div class="l-inner">
+      <p class="p-page-mvText__content">
+      <p class="p-page-mvText__main">現在配布しています瓦製品カタログ、リーフレットをPDFファイル形式で記載しています。<br>
+        ダウンロードしてご活用ください。</p>
+      <p class="p-page-mvText__small">※PDFファイルをご覧いただくためにはアドビ社のAdobe Reader等の専用ソフトが必要になる場合があります</p>
+      </p>
+    </div>
+  </div>
+
+  <section class="p-leaflet">
+    <div class="l-inner">
+      <div class="p-leaflet__content">
+        <div class="p-leaflet__title">
+          <h3 class="c-pageTitle">
+            <span class="c-pageTitle__title-text">屋根材比較</span>
+            <span class="c-pageTitle__title-main">石州瓦の魅力を<br>コンパクトに。</span>
+          </h3>
+        </div>
+
+        <div class="p-leaflet__cards">
+          <a href="#" class="p-leaflet__card">
+            <figure class="p-leaflet__card-image">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note.png" alt="やねノート" width="212" height="300">
+            </figure>
+            <div class="p-leaflet__card-divider"></div>
+            <div class="p-leaflet__card-content">
+              <p class="p-leaflet__card-subtitle">石州瓦ってスゴい！</p>
+              <h4 class="p-leaflet__card-title">やねノート</h4>
+            </div>
+            <div class="p-leaflet__card-button">
+              <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+              <span class="p-leaflet__card-button-icon">
+
+              </span>
+            </div>
+          </a>
+          <a href="#" class="p-leaflet__card">
+            <figure class="p-leaflet__card-image">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note2.png" alt="石州瓦はメンテナンスフリー" width="212" height="300">
+            </figure>
+            <div class="p-leaflet__card-divider"></div>
+            <div class="p-leaflet__card-content">
+              <p class="p-leaflet__card-subtitle">石州瓦はメンテナンスフリー</p>
+              <h4 class="p-leaflet__card-title">石州瓦vs<br>金属・ストレート屋根</h4>
+            </div>
+            <div class="p-leaflet__card-button">
+              <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+              <span class="p-leaflet__card-button-icon">
+
+              </span>
+            </div>
+          </a>
+          <a href="#" class="p-leaflet__card">
+            <figure class="p-leaflet__card-image">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note3.png" alt="石州瓦はエコノミー&エコロジー" width="212" height="300">
+            </figure>
+            <div class="p-leaflet__card-divider"></div>
+            <div class="p-leaflet__card-content">
+              <p class="p-leaflet__card-subtitle">石州瓦はエコノミー&エコロジー</p>
+              <h4 class="p-leaflet__card-title">石州瓦と電気代の<br>
+                うれしい関係</h4>
+            </div>
+            <div class="p-leaflet__card-button">
+              <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+              <span class="p-leaflet__card-button-icon">
+
+              </span>
+            </div>
+          </a>
+          <a href="#" class="p-leaflet__card">
+            <figure class="p-leaflet__card-image">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note4.png" alt="家の耐震化をお考えの方、必見！" width="212" height="300">
+            </figure>
+            <div class="p-leaflet__card-divider"></div>
+            <div class="p-leaflet__card-content">
+              <p class="p-leaflet__card-subtitle">家の耐震化をお考えの方、必見！</p>
+              <h4 class="p-leaflet__card-title">地震に強い家って？</h4>
+            </div>
+            <div class="p-leaflet__card-button">
+              <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+              <span class="p-leaflet__card-button-icon">
+
+              </span>
+            </div>
+          </a>
+          <a href="#" class="p-leaflet__card">
+            <figure class="p-leaflet__card-image">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note5.png" alt="瓦の大切さ知っていますか？" width="212" height="300">
+            </figure>
+            <div class="p-leaflet__card-divider"></div>
+            <div class="p-leaflet__card-content">
+              <p class="p-leaflet__card-subtitle">瓦の大切さ知っていますか？</p>
+              <h4 class="p-leaflet__card-title">「石州瓦」物語</h4>
+            </div>
+            <div class="p-leaflet__card-button">
+              <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+              <span class="p-leaflet__card-button-icon">
+
+              </span>
+            </div>
+          </a>
+          <a href="#" class="p-leaflet__card">
+            <figure class="p-leaflet__card-image">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note6.png" alt="永く、強く、美しく" width="212" height="300">
+            </figure>
+            <div class="p-leaflet__card-divider"></div>
+            <div class="p-leaflet__card-content">
+              <p class="p-leaflet__card-subtitle">永く、強く、美しく</p>
+              <h4 class="p-leaflet__card-title">石州瓦性能読本</h4>
+            </div>
+            <div class="p-leaflet__card-button">
+              <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+              <span class="p-leaflet__card-button-icon">
+
+              </span>
+            </div>
+          </a>
+        </div>
+        <div class="p-leaflet__block">
+          <p class="p-leaflet__block-title">和瓦</p>
+          <div class="p-leaflet__cards">
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note7.png" alt="機能と美の両立。" width="212" height="300">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <p class="p-leaflet__card-subtitle">機能と美の両立。</p>
+                <h4 class="p-leaflet__card-title">TSルーフ</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note8.png" alt="超抵勾配を実現。" width="212" height="300">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <p class="p-leaflet__card-subtitle">超抵勾配を実現。</p>
+                <h4 class="p-leaflet__card-title">GraSlo</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note9.png" alt="高耐久性で安心。" width="212" height="300">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <p class="p-leaflet__card-subtitle">高耐久性で安心。</p>
+                <h4 class="p-leaflet__card-title">BSルーフ</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note10.png" alt="“和”のさらなる深み" width="212" height="300">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <p class="p-leaflet__card-subtitle">"和"のさらなる深み</p>
+                <h4 class="p-leaflet__card-title">彩り来待</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+
+          </div>
+        </div>
+        <div class="p-leaflet__block">
+          <p class="p-leaflet__block-title">洋瓦</p>
+          <div class="p-leaflet__cards">
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note11.png" alt="デザインが彩る。" width="212" height="300">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <p class="p-leaflet__card-subtitle">デザインが彩る。</p>
+                <h4 class="p-leaflet__card-title">ニューセラECOⅡ</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+
+
+          </div>
+        </div>
+        <div class="p-leaflet__block">
+          <p class="p-leaflet__block-title">平板瓦</p>
+          <div class="p-leaflet__cards">
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note12.png" alt="耐久性の平板瓦。" width="212" height="300">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <p class="p-leaflet__card-subtitle">耐久性の平板瓦。</p>
+                <h4 class="p-leaflet__card-title">FtypeU</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+
+
+          </div>
+        </div>
+        <div class="p-leaflet__block">
+          <p class="p-leaflet__block-title">その他</p>
+          <div class="p-leaflet__cards">
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note13.png" alt="防災乾式工法" width="212" height="300">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <p class="p-leaflet__card-subtitle">防災乾式工法</p>
+                <h4 class="p-leaflet__card-title">MBK</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note14.png" alt="耐震・耐風・安全" width="212" height="300">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <p class="p-leaflet__card-subtitle">耐震・耐風・安全</p>
+                <h4 class="p-leaflet__card-title">防災棟</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note15.png" alt="太陽光パネル取付用のJ型瓦" width="212" height="300">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <p class="p-leaflet__card-subtitle">太陽光パネル取付用のJ型瓦</p>
+                <h4 class="p-leaflet__card-title">シジカ</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note16.png" alt="新・美観" width="212" height="300">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <p class="p-leaflet__card-subtitle">新・美観</p>
+                <h4 class="p-leaflet__card-title">セラミックサンド</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+            <a href="#" class="p-leaflet__card">
+              <figure class="p-leaflet__card-image">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note17.png" alt="新・美観" width="212" height="300">
+              </figure>
+              <div class="p-leaflet__card-divider"></div>
+              <div class="p-leaflet__card-content">
+                <p class="p-leaflet__card-subtitle">瓦で作った外構舗装材</p>
+                <h4 class="p-leaflet__card-title">セラテール</h4>
+              </div>
+              <div class="p-leaflet__card-button">
+                <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+                <span class="p-leaflet__card-button-icon">
+
+                </span>
+              </div>
+            </a>
+
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <section class="p-leaflet p-leaflet--last-child">
+    <div class="l-inner">
+      <div class="p-leaflet__content p-leaflet__content--last-child">
+        <div class="p-leaflet__title">
+          <h3 class="c-pageTitle">
+            <span class="c-pageTitle__title-text">屋根材比較</span>
+            <span class="c-pageTitle__title-main">多彩なスタイルに応える<br>製品案内。</span>
+          </h3>
+        </div>
+
+        <div class="p-leaflet__cards">
+          <a href="#" class="p-leaflet__card">
+            <figure class="p-leaflet__card-image">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/download/leaflet-yane-note18.png" alt="石州瓦製品に関するお願い" width="212" height="300">
+            </figure>
+            <div class="p-leaflet__card-divider"></div>
+            <div class="p-leaflet__card-content">
+              <p class="p-leaflet__card-subtitle">石州瓦製品に関するお願い</p>
+              <h4 class="p-leaflet__card-title">粘土瓦の特性</h4>
+            </div>
+            <div class="p-leaflet__card-button">
+              <span class="p-leaflet__card-button-text">DOWNLOAD</span>
+              <span class="p-leaflet__card-button-icon">
+
+              </span>
+            </div>
+          </a>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+  <?php get_template_part("includes/submit"); ?>
+
+</main>
+
+<?php get_footer() ?>
