fa60b3664311a7f2633385eb8958b000b80a6385
yumauda <firststep-yumauda@gmail.com>
2026-01-05 15:18:42 +0900

    アスベスト不使用ページ

---
+++ page-noasbestos.php
@@ -0,0 +1,71 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv p-page-mv--download">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail" data-title="Technical">
+          <h2 class="p-page-mv__title">アスベスト不使用</h2>
+          <p class="p-page-mv__text">For Professional</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-munsell">
+    <div class="l-inner">
+      <div class="p-munsell__content">
+        <div class="p-munsell__title-wrapper">
+          <p class="p-munsell__title-en">Noasbestos</p>
+          <h3 class="p-munsell__title-ja">石綿(アスベスト)不使用について</h3>
+        </div>
+        <div class="p-munsell__text-wrapper">
+          <p class="p-munsell__text">当社製造瓦は粘土瓦であり、粘土瓦は、原材料に石綿(アスベスト)を必要としない建材として、経済産業省及び国土交通省のページに記載されております。<br>
+            原料となる粘土は採掘現場で品質をチェックし、良質な原料粘土のみを使用しています。<br>
+            従って当社製造瓦は、原材料に石綿(アスベスト)を一切使用しておりません。<br>
+            また、他の屋根材のように過去において、原材料に石綿(アスベスト)を使用したということも一切ございません。</p>
+        </div>
+        <div class="p-asbestos">
+          <figure class="p-asbestos__img">
+            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/noasbestos/noasbestos-img.png" alt="石綿(アスベスト)不使用" width="750" height="900">
+          </figure>
+          <div class="p-asbestos__btn-wrapper">
+            <a href="#" class="p-asbestos__btn">
+              <div class="p-asbestos__btn-icon">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/noasbestos/pdf.png" alt="PDFダウンロード" width="20" height="27">
+              </div>
+              <p class="p-asbestos__btn-text">PDFダウンロード</p>
+            </a>
+          </div>
+          <div class="p-noncombustible__text-wrapper">
+            <p class="p-noncombustible__text">参考までに<br>
+              不燃材料認定試験は、試験体を720度にて20分間保持し、試験体に亀裂の発生や有毒ガスが発生しないか確認し、不燃材かどうかの判定をしています。粘土瓦以外のセメント瓦や化粧スレートが各製品毎に不燃材の認定を取得する必要があるのは、表面に塗装した塗料が燃焼し、有毒ガスが発生する可能性や試験体に亀裂が発生する可能性があるからです。<br>
+              また、粘土瓦の表面着色層は、釉薬というガラス状の層であり焼成によって形成されます。そのため釉薬自体も不燃です。</p>
+          </div>
+          <div class="p-related-links">
+            <div class="p-related-links__header">
+              <h3 class="p-related-links__title">関連リンク</h3>
+            </div>
+            <div class="p-related-links__list">
+              <a href="#" class="p-related-links__item" target="_blank" rel="noopener noreferrer">
+                <p class="p-related-links__item-title">石綿(アスベスト)含有建材データベース</p>
+                <p class="p-related-links__item-subtitle">/ 経済産業省及び国土交通省</p>
+              </a>
+              <a href="#" class="p-related-links__item" target="_blank" rel="noopener noreferrer">
+                <p class="p-related-links__item-title">アスベスト非含有建材</p>
+                <p class="p-related-links__item-subtitle">/ 経済産業省及び国土交通省</p>
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
+    </div>
+  </section>
+
+
+  <?php get_template_part("includes/submit"); ?>
+
+</main>
+
+<?php get_footer() ?>
---
+++ page-noncombustible.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv p-page-mv--download">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail" data-title="Technical">
+          <h2 class="p-page-mv__title">不燃材認定について</h2>
+          <p class="p-page-mv__text">For Professional</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-munsell">
+    <div class="l-inner">
+      <div class="p-munsell__content">
+        <div class="p-munsell__title-wrapper">
+          <p class="p-munsell__title-en">Noncombustible</p>
+          <h3 class="p-munsell__title-ja">不燃材認定について</h3>
+        </div>
+        <div class="p-munsell__text-wrapper">
+          <p class="p-munsell__text">当社製品は全て粘土瓦であり、建築基準法第2条第9項に基づき、国土交通省告示1400号にて不燃材として定義されています。<br>
+            当社では、焼成工程において1200度以上にて焼成し製品化しておりますので、全ての製品が不燃材料となります。<br>
+            建築確認申請の不燃材料国土交通大臣認定番号は、「国土交通省告示1400号にて認定」とご記載下さい。</p>
+        </div>
+        <div class="p-noncombustible">
+          <div class="p-noncombustible__title-wrapper">
+            <h3 class="p-noncombustible__title">瓦の不燃材料に係る大臣認定番号の照会について</h3>
+          </div>
+          <div class="p-noncombustible__text-wrapper">
+            <p class="p-noncombustible__text">瓦は、建築基準法（昭和25年法第201号）第2条第9項に基づき、平成12年5月30日建設省告示第1400号（最終改正：平成16年9月29日国土交通省告示第1178号）において不燃材として定められています。よって、不燃材料に係る大臣認定取得は必要ありませんので、建築確認申請の際の不燃材料国土交通省大臣認定番号は、「建設省告示第1400号にて認定」とご記載下さい。<br>
+              当社製品は全て1200度以上の高温で焼成された粘土瓦であり、告示の「瓦」に該当します。建築基準法指定の安全な"不燃材料"で火事による屋根からの類焼を防ぎます。</p>
+          </div>
+          <div class="p-noncombustible__image-wrapper">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/noncombustible/noncombustible-image.png" alt="建設省告示1400号">
+          </div>
+          <div class="p-noncombustible__text-wrapper">
+            <p class="p-noncombustible__text">参考までに<br>
+              不燃材料認定試験は、試験体を720度にて20分間保持し、試験体に亀裂の発生や有毒ガスが発生しないか確認し、不燃材かどうかの判定をしています。粘土瓦以外のセメント瓦や化粧スレートが各製品毎に不燃材の認定を取得する必要があるのは、表面に塗装した塗料が燃焼し、有毒ガスが発生する可能性や試験体に亀裂が発生する可能性があるからです。<br>
+              また、粘土瓦の表面着色層は、釉薬というガラス状の層であり焼成によって形成されます。そのため釉薬自体も不燃です。</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+  <?php get_template_part("includes/submit"); ?>
+
+</main>
+
+<?php get_footer() ?>
