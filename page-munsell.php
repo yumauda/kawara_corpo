4a98c1dc71355e467017d2659bd04c036388305c
yumauda <firststep-yumauda@gmail.com>
2026-01-05 14:52:39 +0900

    マンセル値ページ

---
+++ page-munsell.php
@@ -0,0 +1,213 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv p-page-mv--download">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail" data-title="Technical">
+          <h2 class="p-page-mv__title">マンセル値</h2>
+          <p class="p-page-mv__text">For Professional</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-munsell">
+    <div class="l-inner">
+      <div class="p-munsell__content">
+        <div class="p-munsell__title-wrapper">
+          <p class="p-munsell__title-en">Munsell</p>
+          <h3 class="p-munsell__title-ja">マンセル値</h3>
+        </div>
+        <div class="p-munsell__text-wrapper">
+          <p class="p-munsell__text">近年全国各地で景観条例や街並み保存が実施され、各当地域での建築確認の時点で屋根の色のマンセル値を要求されることが多くなりました。<br>
+            Ｊ形瓦、Ｓ形瓦、Ｆ形瓦のマンセル値を記載しています。<br>
+            標準在庫色以外の色も参考として、表記されている場合がございますのでご注意ください。</p>
+        </div>
+        <div class="p-munsell__table-wrapper">
+          <div class="p-munsell__table-header">
+            <p class="p-munsell__table-header-text">&lt;J形瓦&gt;TSルーフ、グラスロ、BSルーフ、利休</p>
+          </div>
+          <table class="p-munsell__table">
+            <thead class="p-munsell__table-head">
+              <tr>
+                <th class="p-munsell__table-th">色</th>
+                <th class="p-munsell__table-th">色相</th>
+                <th class="p-munsell__table-th">明度</th>
+                <th class="p-munsell__table-th">彩度</th>
+              </tr>
+            </thead>
+            <tbody class="p-munsell__table-body">
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">イブシ</td>
+                <td class="p-munsell__table-td">2.7Y</td>
+                <td class="p-munsell__table-td">4.9</td>
+                <td class="p-munsell__table-td">0.3</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">銀黒</td>
+                <td class="p-munsell__table-td">1.9P</td>
+                <td class="p-munsell__table-td">4.4</td>
+                <td class="p-munsell__table-td">0.3</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">鉄砂（鉄砂）</td>
+                <td class="p-munsell__table-td">7.5YR</td>
+                <td class="p-munsell__table-td">2.4</td>
+                <td class="p-munsell__table-td">0.1</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">来待（きまち）</td>
+                <td class="p-munsell__table-td">4.6YR</td>
+                <td class="p-munsell__table-td">4.0</td>
+                <td class="p-munsell__table-td">2.0</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">赤</td>
+                <td class="p-munsell__table-td">3.1YR</td>
+                <td class="p-munsell__table-td">3.6</td>
+                <td class="p-munsell__table-td">4.1</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">赤来待</td>
+                <td class="p-munsell__table-td">1.2YR</td>
+                <td class="p-munsell__table-td">4.0</td>
+                <td class="p-munsell__table-td">3.0</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">茶来待</td>
+                <td class="p-munsell__table-td">4.3YR</td>
+                <td class="p-munsell__table-td">4.2</td>
+                <td class="p-munsell__table-td">1.4</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">黒来待</td>
+                <td class="p-munsell__table-td">3.1YR</td>
+                <td class="p-munsell__table-td">3.8</td>
+                <td class="p-munsell__table-td">0.7</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">チョコ</td>
+                <td class="p-munsell__table-td">9.5R</td>
+                <td class="p-munsell__table-td">2.9</td>
+                <td class="p-munsell__table-td">2.4</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="p-munsell__table-wrapper">
+          <div class="p-munsell__table-header">
+            <p class="p-munsell__table-header-text">&lt;S形瓦&gt;ニューセラECOⅡ</p>
+          </div>
+          <table class="p-munsell__table">
+            <thead class="p-munsell__table-head">
+              <tr>
+                <th class="p-munsell__table-th">色</th>
+                <th class="p-munsell__table-th">色相</th>
+                <th class="p-munsell__table-th">明度</th>
+                <th class="p-munsell__table-th">彩度</th>
+              </tr>
+            </thead>
+            <tbody class="p-munsell__table-body">
+
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">銀黒</td>
+                <td class="p-munsell__table-td">9.3PB</td>
+                <td class="p-munsell__table-td">4.6</td>
+                <td class="p-munsell__table-td">0.4</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">マットブラック</td>
+                <td class="p-munsell__table-td">7.4YB</td>
+                <td class="p-munsell__table-td">3.1</td>
+                <td class="p-munsell__table-td">0.2</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">マットブラウン</td>
+                <td class="p-munsell__table-td">1.0YR</td>
+                <td class="p-munsell__table-td">3.0</td>
+                <td class="p-munsell__table-td">0.5</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">檜皮（ひわだ）</td>
+                <td class="p-munsell__table-td">2.1YR</td>
+                <td class="p-munsell__table-td">3.8</td>
+                <td class="p-munsell__table-td">2.4</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">白樺（しらかば）</td>
+                <td class="p-munsell__table-td">3.5Y</td>
+                <td class="p-munsell__table-td">6.7</td>
+                <td class="p-munsell__table-td">2.1</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">楓（かえで）</td>
+                <td class="p-munsell__table-td">3.7YR</td>
+                <td class="p-munsell__table-td">4.7</td>
+                <td class="p-munsell__table-td">5.1</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">花梨（かりん）</td>
+                <td class="p-munsell__table-td">6.2YR</td>
+                <td class="p-munsell__table-td">5.4</td>
+                <td class="p-munsell__table-td">4.3</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">サミットレッド</td>
+                <td class="p-munsell__table-td">3.7YR</td>
+                <td class="p-munsell__table-td">4.7</td>
+                <td class="p-munsell__table-td">5.1</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="p-munsell__table-wrapper">
+          <div class="p-munsell__table-header">
+            <p class="p-munsell__table-header-text">&lt;F形瓦&gt;ニューセラF Uタイプ・Fタイプ</p>
+          </div>
+          <table class="p-munsell__table">
+            <thead class="p-munsell__table-head">
+              <tr>
+                <th class="p-munsell__table-th">色</th>
+                <th class="p-munsell__table-th">色相</th>
+                <th class="p-munsell__table-th">明度</th>
+                <th class="p-munsell__table-th">彩度</th>
+              </tr>
+            </thead>
+            <tbody class="p-munsell__table-body">
+
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">銀黒</td>
+                <td class="p-munsell__table-td">2.0P</td>
+                <td class="p-munsell__table-td">4.5</td>
+                <td class="p-munsell__table-td">0.3</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">マットブラック</td>
+                <td class="p-munsell__table-td">3.8RP</td>
+                <td class="p-munsell__table-td">3.3</td>
+                <td class="p-munsell__table-td">0.1</td>
+              </tr>
+              <tr class="p-munsell__table-row">
+                <td class="p-munsell__table-td">マットブラウン</td>
+                <td class="p-munsell__table-td">4.3R</td>
+                <td class="p-munsell__table-td">2.8</td>
+                <td class="p-munsell__table-td">0.9</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="p-munsell__note">
+          <p class="p-munsell__note-text">&lt; 注意事項&gt;<br>
+            マンセル値は参考値ですので、同一マンセル値であっても色見本帳によって色が異なる場合があります。例えば、日本塗料工業会の塗料用標準色と、他の色見本帳の同じマンセル値の色票を比較しても、色が一致しない場合がありますのでご注意ください。<br>
+            無断での転載・転用を固く禁じます。</p>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+  <?php get_template_part("includes/submit"); ?>
+
+</main>
+
+<?php get_footer() ?>
