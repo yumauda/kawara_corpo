f95f246b1d347429266a66abc81d89cfb41730ca
yumauda <firststep-yumauda@gmail.com>
2026-02-18 15:16:06 +0900

    Implement pagination for news archive with updated styles for pager component

--- archive-news.php
+++ archive-news.php
@@ -34,6 +34,114 @@
               <?php endwhile; ?>
             <?php endif; ?>
           </ul>
+
+          <?php
+          global $wp_query;
+
+          $paged = max(1, get_query_var('paged'));
+          $max   = (int) $wp_query->max_num_pages;
+
+          if ($max > 1) :
+            $base = str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999)));
+          ?>
+            <ul class="p-top-news__pager p-pager">
+              <li class="p-pager__list">
+                <?php if ($paged > 1) : ?>
+                  <a href="<?php echo esc_url(get_pagenum_link($paged - 1)); ?>" class="p-pager__link" aria-label="前のページへ">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80">
+                      <g transform="translate(1054 4557)">
+                        <circle cx="40" cy="40" r="40" transform="translate(-1054 -4557)" fill="#37af32" />
+                        <g transform="translate(-479.911 -4524)">
+                          <path d="M197.089-2035.115l-8,7,8,7" transform="translate(-719.177 2035.114)" fill="none" stroke="#222" stroke-width="3" />
+                          <path d="M197.089-2035.115l-8,7,8,7" transform="translate(-727.177 2035.114)" fill="none" stroke="#222" stroke-width="3" />
+                          <path d="M197.089-2035.115l-8,7,8,7" transform="translate(-735.177 2035.114)" fill="none" stroke="#222" stroke-width="3" />
+                        </g>
+                      </g>
+                    </svg>
+                  </a>
+                <?php else : ?>
+                  <span class="p-pager__link" aria-disabled="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80">
+                      <g transform="translate(1054 4557)">
+                        <circle cx="40" cy="40" r="40" transform="translate(-1054 -4557)" fill="#37af32" />
+                        <g transform="translate(-479.911 -4524)">
+                          <path d="M197.089-2035.115l-8,7,8,7" transform="translate(-719.177 2035.114)" fill="none" stroke="#222" stroke-width="3" />
+                          <path d="M197.089-2035.115l-8,7,8,7" transform="translate(-727.177 2035.114)" fill="none" stroke="#222" stroke-width="3" />
+                          <path d="M197.089-2035.115l-8,7,8,7" transform="translate(-735.177 2035.114)" fill="none" stroke="#222" stroke-width="3" />
+                        </g>
+                      </g>
+                    </svg>
+                  </span>
+                <?php endif; ?>
+              </li>
+
+              <ul class="p-pager__number">
+                <?php
+                $links = paginate_links([
+                  'base'      => $base,
+                  'format'    => '',
+                  'current'   => $paged,
+                  'total'     => $max,
+                  'mid_size'  => 1,
+                  'end_size'  => 1,
+                  'type'      => 'array',
+                  'prev_next' => false,
+                ]);
+
+                if (!empty($links)) :
+                  foreach ($links as $link_html) :
+                    $is_current = (strpos($link_html, 'current') !== false);
+
+                    if ($is_current) {
+                      $page_num = strip_tags($link_html);
+                      echo '<li class="p-pager__list-num"><span class="p-pager__link-num is-current" aria-current="page">'
+                        . esc_html(sprintf('%02d', (int) $page_num))
+                        . '</span></li>';
+                    } else {
+                      preg_match('/href=[\'"]([^\'"]+)[\'"]/', $link_html, $m);
+                      $href = $m[1] ?? '#';
+                      $page_num = strip_tags($link_html);
+
+                      echo '<li class="p-pager__list-num"><a href="' . esc_url($href) . '" class="p-pager__link-num">'
+                        . esc_html(sprintf('%02d', (int) $page_num))
+                        . '</a></li>';
+                    }
+                  endforeach;
+                endif;
+                ?>
+              </ul>
+
+              <li class="p-pager__list">
+                <?php if ($paged < $max) : ?>
+                  <a href="<?php echo esc_url(get_pagenum_link($paged + 1)); ?>" class="p-pager__link" aria-label="次のページへ">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80">
+                      <g transform="translate(1054 4557)">
+                        <circle cx="40" cy="40" r="40" transform="translate(-1054 -4557)" fill="#37af32" />
+                        <g transform="translate(-1564.089 -6978.886)">
+                          <path d="M189.089-2035.115l8,7-8,7" transform="translate(365 4490)" fill="none" stroke="#222" stroke-width="3" />
+                          <path d="M189.089-2035.115l8,7-8,7" transform="translate(357 4490)" fill="none" stroke="#222" stroke-width="3" />
+                          <path d="M189.089-2035.115l8,7-8,7" transform="translate(349 4490)" fill="none" stroke="#222" stroke-width="3" />
+                        </g>
+                      </g>
+                    </svg>
+                  </a>
+                <?php else : ?>
+                  <span class="p-pager__link" aria-disabled="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80">
+                      <g transform="translate(1054 4557)">
+                        <circle cx="40" cy="40" r="40" transform="translate(-1054 -4557)" fill="#37af32" />
+                        <g transform="translate(-1564.089 -6978.886)">
+                          <path d="M189.089-2035.115l8,7-8,7" transform="translate(365 4490)" fill="none" stroke="#222" stroke-width="3" />
+                          <path d="M189.089-2035.115l8,7-8,7" transform="translate(357 4490)" fill="none" stroke="#222" stroke-width="3" />
+                          <path d="M189.089-2035.115l8,7-8,7" transform="translate(349 4490)" fill="none" stroke="#222" stroke-width="3" />
+                        </g>
+                      </g>
+                    </svg>
+                  </span>
+                <?php endif; ?>
+              </li>
+            </ul>
+          <?php endif; ?>
         </div>
       </div>
     </div>
